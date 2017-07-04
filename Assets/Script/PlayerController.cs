7d0e69076fa24bff08699b6e867455e5478acc65
Stockthin <stockthin@gmail.com>
2017-07-04 16:58:45 +0700

    7

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerControllesr : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
     public float moveForce = 5;
     Rigidbody2D myBD;
 	// Use this for initialization
 	void Start () {
         myBD = this.GetComponent<Rigidbody2D>();
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") ,CrossPlatformInputManager.GetAxis("Vertical") * moveForce);
-        myBD.AddForce(movevec);
-       /* Vector2 scale = transform.localScale;
-            scale.x = (float)-0.4;
-            transform.localScale = scale; */
+        Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") ,CrossPlatformInputManager.GetAxisRaw("Vertical") * moveForce);
+
+        transform.Translate(new Vector2(0, movevec.y * Time.deltaTime));
+        
+
+        //myBD.AddForce(movevec);
+        /* Vector2 scale = transform.localScale;
+             scale.x = (float)-0.4;
+             transform.localScale = scale; */
     }
 }
