951ea037e71c684d7e393fe627ef4ec269d47197
Stockthin <stockthin@gmail.com>
2017-07-05 14:32:22 +0700

    10

---
+++ Assets/Script/enemyDamage.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyDamage : MonoBehaviour {
+    public float damage;
+    public float dameRate;
+    //public float pushBackForce;
+    float NextDamage;
+    public GameObject expl;
+    // Use this for initialization
+    void Start () {
+        NextDamage = 0f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        
+        if (coll.gameObject.tag == "Player" && NextDamage < Time.time)
+        {
+            playerHealth thePlayerHealth = coll.gameObject.GetComponent<playerHealth>();
+            thePlayerHealth.addDame(damage);
+            NextDamage = Time.time + dameRate;
+            Destroy(this.gameObject);
+            Instantiate(expl, transform.position, Quaternion.identity);
+            //pushBack(coll.transform);
+            Debug.Log("dadsafsa");
+        }
+    }
+   /* void pushBack(Transform pushedObject)
+    {
+        Vector2 pushDirection = new Vector2(0, (pushedObject.position.y - transform.position.y)).normalized;
+        pushDirection *= pushBackForce;
+        Rigidbody2D pushRB = pushedObject.gameObject.GetComponent<Rigidbody2D>();
+        pushRB.velocity = Vector2.zero;
+        pushRB.AddForce(pushDirection, ForceMode2D.Impulse);
+    }*/
+}
---
+++ Assets/Script/playerHealth.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour {
+    public float fullHealth;
+    float currentHealth;
+   //playerController controlMovement;
+    public GameObject DeathFX;
+    public Slider healthSlider;
+    //public Image damageScreen;
+    bool damaged = false;
+   //Color damagedColor = new Color(0f, 0f, 0f, 0.5f);
+    //float smoothColour = 5f;
+    public AudioClip playerHurt;
+   AudioSource playerAS;
+  // public AudioClip playerDeathAudio;
+   // public Text gameOverScreen;
+   //public restartGame theGameManager;
+    
+    
+    // Use this for initialization
+    void Start () {
+        
+        currentHealth = fullHealth;
+        //controlMovement = GetComponent<playerController>();
+        healthSlider.maxValue = fullHealth;
+        healthSlider.value = fullHealth;
+        damaged = false;
+        playerAS = GetComponent<AudioSource>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (damaged)
+        {
+            
+           //damageScreen.color = damagedColor;
+
+        }
+        else
+        {
+           //damageScreen.color = Color.Lerp(damageScreen.color, Color.clear, smoothColour * Time.deltaTime);
+        }
+        damaged = false;
+	
+	}
+    public void addDame(float Damage)
+    {
+        if (Damage <= 0) return;
+        currentHealth -= Damage;
+        playerAS.clip = playerHurt;
+        playerAS.Play();
+       playerAS.PlayOneShot(playerHurt);
+        healthSlider.value = currentHealth;
+        damaged = true;
+        if (currentHealth <= 0)
+        {
+           
+            makeDead();
+           
+        }
+    }
+    public void addHealth(float healthAmount)
+    {
+        currentHealth += healthAmount;
+        if (currentHealth > fullHealth) currentHealth = fullHealth;
+        healthSlider.value = currentHealth;
+    }
+    public void makeDead()
+    {
+        //AudioSource.PlayClipAtPoint(playerDeathAudio, transform.position);
+        Instantiate(DeathFX, transform.position, transform.rotation);
+        Destroy(gameObject);
+        //playerAS.PlayOneShot(playerDeathAudio);
+        //
+        // damageScreen.color = damagedColor;
+        // Animator gameOverAnimator = gameOverScreen.GetComponent<Animator>();
+        // gameOverAnimator.SetTrigger("gameOver");
+        //theGameManager.restartTheGame();
+
+    }
+}
