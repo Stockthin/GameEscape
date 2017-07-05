b1d5c158bc802d70b71b775ca792edf20ff74964
Stockthin <stockthin@gmail.com>
2017-07-05 11:00:35 +0700

    9

---
+++ Assets/Script/DestroyPa.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPa : MonoBehaviour {
+
+    
+    public float timeLife;
+    void OnEnable()
+    {
+        Invoke("Destroy", timeLife);
+       // StartCoroutine(IEDestroy());
+    }
+
+     /*IEnumerator IEDestroy()
+     {
+         yield return new WaitForSeconds(0.1);
+
+         Destroy();
+     }*/
+    void Destroy()
+    {
+        //gameObject.SetActive(false);
+        Destroy(this.gameObject);
+        
+    }
+    void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
---
+++ Assets/TimeControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour
+{
+    private Slider timeSlider;
+    private GameObject player;
+    public float time;
+    public float timeBurn;
+    public float timeLeft = 1f;
+    void Awake()
+    {
+        GetPrefereces();
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player)
+        {
+            return;
+        }
+        if (time > 0)
+        {
+            time -= timeBurn * Time.deltaTime * timeLeft;
+            timeSlider.value = time;
+        }
+        else
+        {
+            Destroy(player);
+        }
+    }
+    void GetPrefereces()
+    {
+        player = GameObject.Find("Player");
+        timeSlider = GameObject.Find("Time").GetComponent<Slider>();
+        timeSlider.minValue = 0;
+        timeSlider.maxValue = time;
+        timeSlider.value = timeSlider.maxValue;
+    }
+}
