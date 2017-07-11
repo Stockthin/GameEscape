30fd69ee1e76ecc51bc37e8e7256c1f131ba33c7
Stockthin <stockthin@gmail.com>
2017-07-11 16:57:59 +0700

    fsd

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -3,40 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerController : MonoBehaviour {
-    public float moveForce = 5;
-    Rigidbody2D myBD;
-    public GameObject bullet;
-    public Transform shotFrom;
-    public float shootTime;
-    public int changeShoot;
-    float nextShootTime;
-    // Use this for initialization
-    void Start () {
-        myBD = this.GetComponent<Rigidbody2D>();
+public class PlayerController : MonoBehaviour
+{
+	public float moveForce = 5;
+	Rigidbody2D myBD;
+	public GameObject bullet;
+	public Transform shotFrom;
+	public float shootTime;
+	public int changeShoot;
+	float nextShootTime;
+	// Use this for initialization
+	void Start ()
+	{
+		myBD = this.GetComponent<Rigidbody2D> ();
 
-    }
-    void fire()
-    {
-        if (Input.GetKey(KeyCode.Space) && nextShootTime < Time.time)
-        {
-            nextShootTime = Time.time + shootTime;
+	}
+
+	public void fire ()
+	{
+		if (Input.GetKey (KeyCode.Space) && nextShootTime < Time.time) {
+			nextShootTime = Time.time + shootTime;
             
-            Instantiate(bullet, shotFrom.position,Quaternion.identity);
-            
-        }
-    }
+			GameObject shuriken = Instantiate (bullet, shotFrom.position, Quaternion.identity).gameObject;
 
-    // Update is called once per frame
-    void Update () {
-        Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") ,CrossPlatformInputManager.GetAxisRaw("Vertical") * moveForce);
+			float shootForce = 5;
 
-        transform.Translate(new Vector2(0, movevec.y * Time.deltaTime));
-        fire();
+			if (transform.localScale.x > 0)
+				shuriken.GetComponent<ShurikenControl> ().Shoot (new Vector2 (shootForce, 0));
+			else
+				shuriken.GetComponent<ShurikenControl> ().Shoot (new Vector2 (-shootForce, 0));
+		}
+	}
 
-        //myBD.AddForce(movevec);
-        /* Vector2 scale = transform.localScale;
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector2 movevec = new Vector2 (CrossPlatformInputManager.GetAxis ("Horizontal"), CrossPlatformInputManager.GetAxisRaw ("Vertical") * moveForce);
+
+		transform.Translate (new Vector2 (0, movevec.y * Time.deltaTime));
+		fire ();
+
+		//myBD.AddForce(movevec);
+		/* Vector2 scale = transform.localScale;
              scale.x = (float)-0.4;
              transform.localScale = scale; */
-    }
+	}
 }
--- Assets/Script/Player/ShurikenControl.cs
+++ Assets/Script/Player/ShurikenControl.cs
@@ -2,33 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShurikenControl : MonoBehaviour {
-    public float shurikenSpeed;
-    Rigidbody2D myRB;
-   
+public class ShurikenControl : MonoBehaviour
+{
+	public float shurikenSpeed;
+	Rigidbody2D myRB;
+
   
-    void Awake()
-    {
+	void Awake ()
+	{
+		myRB = GetComponent<Rigidbody2D> ();
+		//myRB.AddForce(transform.forward * shurikenSpeed);
+//		if (transform.localRotation.z > 0)
+//			myRB.AddForce (new Vector2 (-1, 0) * shurikenSpeed, ForceMode2D.Impulse);
+//		else
+//			myRB.AddForce (new Vector2 (1, 0) * shurikenSpeed, ForceMode2D.Impulse);
         
 
-        myRB = GetComponent<Rigidbody2D>();
-        //myRB.AddForce(transform.forward * shurikenSpeed);
-        if (transform.localRotation.z>0)
-            myRB.AddForce(new Vector2(-1, 0) * shurikenSpeed, ForceMode2D.Impulse);
-        else myRB.AddForce(new Vector2(1, 0) * shurikenSpeed, ForceMode2D.Impulse);
-        
+	}
 
-    }
+	public void Shoot (Vector2 force)
+	{
+		myRB.AddForce (force, ForceMode2D.Impulse);
+	}
 
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        //transform.Translate(Vector3.forward * Time.deltaTime * shurikenSpeed);
-        //myRB.AddForce(transform.forward * shurikenSpeed);
-    }
+	void Update ()
+	{
+		//transform.Translate(Vector3.forward * Time.deltaTime * shurikenSpeed);
+		//myRB.AddForce (transform.forward * shurikenSpeed);
+	}
   
 }
