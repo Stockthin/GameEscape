476fc204385bfe0e5ec1e35e011c6697f8175db9
Stockthin <stockthin@gmail.com>
2017-07-11 13:41:26 +0700

    ád

--- Assets/Script/GameManager/MainMenu.cs
+++ Assets/Script/GameManager/MainMenu.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-
-    public void PlayGame()
+    public GameObject Loading;
+    public Slider slider;
+    public Text progressText;
+    IEnumerator LoadScENEaSync(string index)
     {
-       
-    
-        SceneManager.LoadScene("Play");
-        Time.timeScale = 0f;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+        Loading.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+
+        }
     }
+    public void PlayGame(string index)
+    {
+        StartCoroutine(LoadScENEaSync("Play"));
+        //Time.timeScale = 0f;
+    }
+
     public void Level2()
     {
         SceneManager.LoadScene("level1");
