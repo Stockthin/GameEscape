184dab49651eee304f2edd6b871bef87cffa1400
Stockthin <stockthin@gmail.com>
2017-07-11 16:47:56 +0700

    yt

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour {
     public float moveForce = 5;
     Rigidbody2D myBD;
     public GameObject bullet;
-    //public Transform shotFrom;
+    public Transform shotFrom;
     public float shootTime;
     public int changeShoot;
     float nextShootTime;
@@ -16,34 +16,22 @@ public class PlayerController : MonoBehaviour {
         myBD = this.GetComponent<Rigidbody2D>();
 
     }
-    void fire()  //OnTriggerStay2D(Collider2D other)
+    void fire()
     {
         if (Input.GetKey(KeyCode.Space) && nextShootTime < Time.time)
         {
             nextShootTime = Time.time + shootTime;
             
-            Instantiate(bullet, transform.position, Quaternion.identity);
+            Instantiate(bullet, shotFrom.position,Quaternion.identity);
             
         }
     }
 
     // Update is called once per frame
     void Update () {
-        Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") * moveForce ,CrossPlatformInputManager.GetAxisRaw("Vertical") * moveForce);
-        
-        transform.Translate(new Vector2(movevec.x * Time.deltaTime, movevec.y * Time.deltaTime));
-        if(movevec.x > 0)
-        {
-            Vector3 scale = transform.localScale;
-            scale.x = (float)0.3;
-            transform.localScale = scale;
-        }
-        else
-        {
-            Vector3 scale = transform.localScale;
-            scale.x = (float)-0.3;
-            transform.localScale = scale;
-        }
+        Vector2 movevec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") ,CrossPlatformInputManager.GetAxisRaw("Vertical") * moveForce);
+
+        transform.Translate(new Vector2(0, movevec.y * Time.deltaTime));
         fire();
 
         //myBD.AddForce(movevec);
--- Assets/Script/Player/ShurikenControl.cs
+++ Assets/Script/Player/ShurikenControl.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ShurikenControl : MonoBehaviour {
     public float shurikenSpeed;
     Rigidbody2D myRB;
+   
   
     void Awake()
     {
+        
+
         myRB = GetComponent<Rigidbody2D>();
         //myRB.AddForce(transform.forward * shurikenSpeed);
-        if (transform.localPosition.z > 0)
+        if (transform.localRotation.z>0)
             myRB.AddForce(new Vector2(-1, 0) * shurikenSpeed, ForceMode2D.Impulse);
         else myRB.AddForce(new Vector2(1, 0) * shurikenSpeed, ForceMode2D.Impulse);
         
@@ -24,7 +27,8 @@ public class ShurikenControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //transform.Translate(Vector3.forward * Time.deltaTime * shurikenSpeed);
+        //myRB.AddForce(transform.forward * shurikenSpeed);
+    }
   
 }
