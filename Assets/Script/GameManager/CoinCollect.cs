c2eebbf6bde13fb44ab0ee2c2aff0f7790212feb
Stockthin <stockthin@gmail.com>
2017-08-01 10:53:14 +0700

    fyfyyfufyfyffyu

---
+++ Assets/Script/GameManager/CoinCollect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour {
+    public int coinAmount;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            CoinManager.addPoint(coinAmount);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/GameManager/CoinManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour {
+    public static int coinScore;
+    Text text;
+    public static int totalCoin;
+	// Use this for initialization
+ 
+	void Start () {
+        text = GetComponent<Text>();
+        coinScore = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(coinScore < 0)
+        {
+            coinScore = 0;
+        }
+        text.text = " = " + coinScore;
+        totalCoin += coinScore;
+	}
+    public static void addPoint(int coinAdd)
+    {
+        coinScore += coinAdd;
+    }
+}
